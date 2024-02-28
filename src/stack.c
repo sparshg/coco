26797a0f8f5684923a02890309b17b2f0517bb72
RishiG2506 <clasherrishi@gmail.com>
2024-02-28 10:02:03 +0530

    added Stack ADT

---
+++ src/stack.c
@@ -0,0 +1,70 @@
+#include "stack.h"
+
+
+
+STACK createStack() {
+    STACK stack = (STACK)malloc(sizeof(Stack));
+    stack->data = (int*)malloc(sizeof(int) * MAX_STACK_SIZE);
+    stack->top = -1;
+    return stack;
+}
+
+int isEmpty(STACK stack) {
+    return stack->top == -1;
+}
+
+int size(STACK stack) {
+    return stack->top + 1;
+}
+
+void push(STACK stack, int value) {
+    if(stack->top == MAX_STACK_SIZE - 1) {
+        printf("Stack Overflow\n");
+        return;
+    }
+    stack->data[++stack->top] = value;
+    return;
+}
+
+void pop(STACK stack) {
+    if (isEmpty(stack)) {
+        printf("Stack Underflow\n");
+        return;
+    }
+    stack->data[stack->top--];
+    return;
+}
+
+int top(STACK stack) {
+    if (isEmpty(stack)) {
+        printf("Stack is Empty!\n");
+        return -1;
+    }
+    return stack->data[stack->top];
+}
+
+
+
+
+int main() {
+    STACK stack = createStack();    
+
+    push(stack, 10);
+    push(stack, 20);
+    push(stack, 30);
+    push(stack,40);
+    push(stack,50);
+    push(stack,60);
+
+    printf("Top element: %d\n", top(stack));
+    pop(stack);
+    printf("Popped element: %d\n", top(stack));
+    pop(stack);
+    printf("Popped element: %d\n", top(stack));
+    pop(stack);
+    printf("Popped element: %d\n", top(stack));
+
+    printf("Is stack empty? %s\n", isEmpty(stack) ? "Yes" : "No");
+
+    return 0;
+}
---
+++ src/stack.h
@@ -0,0 +1,25 @@
+#ifndef STACK_H
+#define STACK_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_STACK_SIZE 100
+
+
+typedef struct {
+    int* data;
+    int top;
+} Stack;
+
+typedef Stack* STACK;
+
+STACK createStack();
+void push(STACK stack, int value);
+void pop(STACK stack);
+int top(STACK stack);
+int isEmpty(STACK stack);
+int size(STACK stack);
+
+
+#endif
